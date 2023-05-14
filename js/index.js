46644f552059465d6cd2dbce0d109f202e1ae057
NandoSTI <fernandosmendesti@gmail.com>
2023-05-14 01:16:55 -0300

    terceiro-commit

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 const botaoAlterarTema = document.getElementById("botao-alterar-tema");
 const body = document.querySelector("body");
-botaoAlterarTema.addEventListener("click", () => {
-    const temaDois = body.classList.contains("tema-dois");
-    if(temaDois){
-        body.classList.remove("tema-dois");
-    }else{
-        body.classList.add("tema-dois");
-    }
-});
 
+botaoAlterarTema.addEventListener("click", () => {
+  const temaDois = body.classList.contains("tema-dois");
+ 
+  body.classList.toggle("tema-dois");
+
+});
