9c4d419e4760c936cb444ba4f95ba786107fc2a1
NandoSTI <fernandosmendesti@gmail.com>
2023-05-14 03:56:19 -0300

    quarto-commit

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 const botaoAlterarTema = document.getElementById("botao-alterar-tema");
 const body = document.querySelector("body");
+const vinteNoveVinteCinco = document.querySelector("explicacao");
 
 botaoAlterarTema.addEventListener("click", () => {
   const temaDois = body.classList.contains("tema-dois");
  
   body.classList.toggle("tema-dois");
 
+
+
 });
+
