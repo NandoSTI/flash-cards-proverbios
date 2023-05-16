bf9c53321747a7ff0753b6428b5405c50dcfd8a0
NandoSTI <fernandosmendesti@gmail.com>
2023-05-16 07:10:41 -0300

    quintocommit

--- js/index.js
+++ js/index.js
@@ -1,16 +1,57 @@
 const botaoAlterarTema = document.getElementById("botao-alterar-tema");
-const mostarExplicacao1 = document.getElementById("mostrar-explicacao");
 const body = document.querySelector("body");
+
+const botaoMostrarExplicacao = document.getElementById("mostrar-explicacao");
+const conteudoVersiculo = document.getElementById("conteudo-versiculo");
+
 
 
 botaoAlterarTema.addEventListener("click", () => {
-  const temaDois = body.classList.contains("tema-dois");
-
-  
+  body.classList.contains("tema-dois");
  
   body.classList.toggle("tema-dois");
-  
-  
+
 
 });
+
+
+botaoMostrarExplicacao.addEventListener("click", () => {
+
+
+  if(conteudoVersiculo.style.display === "block"){
+    conteudoVersiculo.style.display = "none";
+    } else{
+    conteudoVersiculo.style.display = "block";
+    }
+
+
+    const card = document.querySelector(".conteudo-versiculo")
+    card.classList.toggle("active")
+
+    if(card.classList.contains("active")) {
+      return botaoMostrarExplicacao.textContent = "Voltar"
+    }
+    
+
+    botaoMostrarExplicacao.textContent = "Entenda o versículo"
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
